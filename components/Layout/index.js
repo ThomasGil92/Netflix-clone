ddbfb1e8ca4a1797695c5c0f2d084d814625ba9f
Thomas Gil <tgil849@gmail.com>
2022-01-13 15:49:03 +0100

    Home page

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 
 const GlobalLayout = ({ children }) => {
   return (
@@ -15,11 +16,11 @@ const GlobalLayout = ({ children }) => {
         ></link>
       </Head>
       {children}
-      <script
+      <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossOrigin="anonymous"
-      ></script>
+      ></Script>
     </div>
   );
 };
