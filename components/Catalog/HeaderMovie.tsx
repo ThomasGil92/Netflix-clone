e78973bd3145a07e8c42fc820e63de12f240a146
Thomas Gil <tgil849@gmail.com>
2023-04-24 13:35:28 +0200

    update

--- components/Catalog/HeaderMovie.tsx
+++ components/Catalog/HeaderMovie.tsx
@@ -27,7 +27,7 @@ const HeaderMovie: React.FC<MovieProps> = ({ movie }) => {
 
   const getTrailerUrl = async () => {
     const trailers: Trailers = await fetch(
-      `${process.env.NEXT_PUBLIC_IMDB_API_URL}/movie/${movie.id}/videos?api_key=${process.env.NEXT_PUBLIC_IMDB_API_KEY}`
+      `${process.env.NEXT_PUBLIC_IMDB_API_URL}/movie/${movie.id}/videos?api_key=${process.env.NEXT_PUBLIC_IMDB_API_KEY}`,
     )
       .then(function (response) {
         return response.text();
@@ -38,7 +38,7 @@ const HeaderMovie: React.FC<MovieProps> = ({ movie }) => {
       });
     const trailer = () => {
       const t = trailers.results.find((e) =>
-        e.name.toLowerCase().includes("official trailer")
+        e.name.toLowerCase().includes("official trailer"),
       );
       return t !== undefined ? t : trailers.results[0];
     };
@@ -53,7 +53,7 @@ const HeaderMovie: React.FC<MovieProps> = ({ movie }) => {
   return (
     <>
       <div
-        className="fixed-top"
+        className='fixed-top'
         style={{
           overflow: "hidden",
         }}
@@ -66,20 +66,20 @@ const HeaderMovie: React.FC<MovieProps> = ({ movie }) => {
             backgroundImage:
               "linear-gradient( to right, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 1) 40%, rgba(0, 0, 0, 0) 60% ",
           }}
-          className="w-100 d-flex align-items-center text-white px-5"
+          className='w-100 d-flex align-items-center text-white px-5'
         >
           <div>
             <div style={{ maxWidth: "50vw", paddingLeft: 12, zIndex: 100000 }}>
               {" "}
-              <h1 className="bigger_h1" style={{ lineHeight: 0.9 }}>
+              <h1 className='bigger_h1' style={{ lineHeight: 0.9 }}>
                 Regarder {movie.title} maintenant
               </h1>
               <p>{movie.overview.split(" ").slice(0, 30).join(" ")} ...</p>
               <button
-                type="button"
-                className="btn-custom-white rounded py-2 px-3"
-                data-bs-toggle="modal"
-                data-bs-target="#FirstMovieHeader"
+                type='button'
+                className='btn-custom-white rounded py-2 px-3'
+                data-bs-toggle='modal'
+                data-bs-target='#FirstMovieHeader'
                 onClick={(e) => {
                   e.preventDefault(), getTrailerUrl();
                 }}
@@ -87,10 +87,10 @@ const HeaderMovie: React.FC<MovieProps> = ({ movie }) => {
                 Regarder la bande annonce
               </button>
               <button
-                type="button"
-                className="btn-custom-dark rounded py-2 px-3"
-                data-bs-toggle="modal"
-                data-bs-target="#MoreInfos"
+                type='button'
+                className='btn-custom-dark rounded py-2 px-3'
+                data-bs-toggle='modal'
+                data-bs-target='#MoreInfos'
                 onClick={(e) => {
                   e.preventDefault(); /* , getTrailerUrl(); */
                 }}
@@ -110,12 +110,12 @@ const HeaderMovie: React.FC<MovieProps> = ({ movie }) => {
               right: 0,
               zIndex: -2,
             }}
-            className="d-flex justify-content-center align-items-center"
+            className='d-flex justify-content-center align-items-center'
           >
-            <div className="loader">Loading...</div>
+            <div className='loader'>Loading...</div>
           </div>
           <img
-            alt="poster"
+            alt='poster'
             src={`${process.env.NEXT_PUBLIC_IMDB_API_IMG_URL}${movie.backdrop_path}`}
             style={{
               backgroundColor: "#000",
@@ -131,26 +131,26 @@ const HeaderMovie: React.FC<MovieProps> = ({ movie }) => {
       </div>
       {/* <!-- Modal --> */}
       <div
-        className="modal fade text-dark"
-        id="FirstMovieHeader"
+        className='modal fade text-dark'
+        id='FirstMovieHeader'
         tabIndex={-1}
-        aria-labelledby="FirstMovieHeaderLabel"
-        aria-hidden="true"
-        data-bs-dismiss="modal"
+        aria-labelledby='FirstMovieHeaderLabel'
+        aria-hidden='true'
+        data-bs-dismiss='modal'
         onClick={(e) => {
           e.preventDefault(), hide();
         }}
       >
-        <div className="modal-dialog modal-lg  modal-dialog-centered">
-          <div className="modal-content">
+        <div className='modal-dialog modal-lg  modal-dialog-centered'>
+          <div className='modal-content'>
             {trailerUrl && (
-              <div className="modal-body p-0 " style={{ maxHeight: 500 }}>
+              <div className='modal-body p-0 ' style={{ maxHeight: 500 }}>
                 <iframe
-                  width="100%"
-                  height="500"
+                  width='100%'
+                  height='500'
                   src={`https://www.youtube.com/embed/${trailerUrl}?autoplay=1&origin=localhost:3000&mute=1`}
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  frameBorder='0'
+                  allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
                   allowFullScreen
                 ></iframe>
               </div>
@@ -160,48 +160,48 @@ const HeaderMovie: React.FC<MovieProps> = ({ movie }) => {
       </div>
 
       <div
-        className="modal fade text-dark"
-        id="MoreInfos"
+        className='modal fade text-dark'
+        id='MoreInfos'
         tabIndex={-1}
-        aria-labelledby="MoreInfosHeaderLabel"
-        aria-hidden="true"
-        data-bs-dismiss="modal"
+        aria-labelledby='MoreInfosHeaderLabel'
+        aria-hidden='true'
+        data-bs-dismiss='modal'
         onClick={(e) => {
           e.preventDefault(), hide();
         }}
       >
         <div
-          className="modal-dialog modal-md m-0 ms-auto modal-dialog-centered"
+          className='modal-dialog modal-md m-0 ms-auto modal-dialog-centered'
           style={{ height: "100vh" }}
         >
           <div
-            id="moreInfosModalBody"
-            className="modal-content"
+            id='moreInfosModalBody'
+            className='modal-content'
             style={{ height: "100vh" }}
           >
             {movie && (
               <>
-                <div className="modal-header border-0">
-                  <h1 className="modal-title" id="exampleModalLabel">
+                <div className='modal-header border-0'>
+                  <h1 className='modal-title' id='exampleModalLabel'>
                     {movie.title}
                   </h1>
                   <button
-                    type="button"
-                    className="btn-close  btn-close-white"
-                    data-bs-dismiss="modal"
-                    aria-label="Close"
+                    type='button'
+                    className='btn-close  btn-close-white'
+                    data-bs-dismiss='modal'
+                    aria-label='Close'
                   ></button>
                 </div>
 
                 <div
-                  className="modal-body px-3 pt-0"
+                  className='modal-body px-3 pt-0'
                   style={{ maxHeight: 500 }}
                 >
                   <h4>Synopsis:</h4>
                   <p>{movie.overview}</p>
-                  <h4 className="mt-4 mb-0">Date de sortie:</h4>
+                  <h4 className='mt-4 mb-0'>Date de sortie:</h4>
                   <p>{movie.release_date}</p>
-                  <h4 className="mt-4 mb-0">Note:</h4>
+                  <h4 className='mt-4 mb-0'>Note:</h4>
                   <p>{movie.vote_average}/10</p>
                 </div>
               </>
